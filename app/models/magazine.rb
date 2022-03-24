cec00d77c0e4e943bfa5be45fa1aeda06d12eee9
Bevin Duncan <bevin.duncan@gmail.com>
2022-03-23 17:50:10 -0700

    Completed Assignment

--- app/models/magazine.rb
+++ app/models/magazine.rb
@@ -1,3 +1,17 @@
 class Magazine < ActiveRecord::Base
   
+has_many :subscriptions 
+  has_many :readers, through: :subscriptions
+
+  def email_list
+#    subscriptions.readers.map do |reader| 
+#     "#{reader.email};"
+#    end
+self.readers.pluck(:email).join(";")
+  end
+
+    def most_popular
+        self.all.max_by{|mag| mag.subscriptions.length}
+    end       
+
 end
--- app/models/reader.rb
+++ app/models/reader.rb
@@ -1,3 +1,19 @@
 class Reader < ActiveRecord::Base
+    has_many :subscriptions
+    has_many :magazines, through: :subscriptions
+  
+    def subscribe(price, magazine)
+      subscriptions.create(price: price, magazine: magazine)  
+    end
+  
+    def total_subscription_price
+      subscriptions.all.sum(:price)
+    end
+  
+    def cancel_subscription(magazine)
+  
+     deleted_magazine = self.subscriptions.find_by(magazine: magazine)
+     deleted_magazine.destroy || "This reader is not subscribed"
+    end 
   
 end
